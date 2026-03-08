65df3c06d52f39426691850195c7bad381c6592b
rainndev <182114243+rainndev@users.noreply.github.com>
2026-03-08 09:32:50 +0800

    feat: update weather UI with SVG icons, improved layout, and enhanced hourly forecast display

--- src/js/WeatherUI.ts
+++ src/js/WeatherUI.ts
@@ -1,4 +1,8 @@
+import { Animate } from "./Animate";
 import { Weather } from "./Weather";
+
+const timeSVG =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="35px"  viewBox="0 0 24 24"><title xmlns="">clock</title><path fill="oklch(0.22 0.05 280/0.9)" d="M19 3H5v2H3v14h2v2h14v-2h2V5h-2zm0 2v14H5V5zm-8 2h2v6h4v2h-6z"/></svg>';
 
 export class WeatherUI {
   weather: Weather;
@@ -9,6 +13,7 @@ export class WeatherUI {
   weatherIconElement: HTMLImageElement;
   humidityElement: HTMLParagraphElement;
   feelsLikeElement: HTMLParagraphElement;
+  locationTitleElement: HTMLHeadingElement;
 
   constructor(weatherService: Weather) {
     this.weather = weatherService;
@@ -22,15 +27,23 @@ export class WeatherUI {
     this.weatherIconElement = $("weather-icon");
     this.humidityElement = $("humidity");
     this.feelsLikeElement = $("feels-like");
+    this.locationTitleElement = $("location-title");
   }
 
   async render() {
-    const weather = await this.weather.getWeather();
+    Animate.decryptionAnimation(
+      this.locationTitleElement,
+      "Pampanga, Philippines",
+    );
 
-    this.currentTempElement.innerText = `${weather.current?.temperature_2m}°C`;
-    this.windSpeedElement.innerText = `Wind Speed: ${weather.current?.wind_speed_10m} km/h`;
-    this.humidityElement.innerText = `Humidity: ${weather.current?.relative_humidity_2m}%`;
-    this.feelsLikeElement.innerText = `Feels Like: ${weather.current?.apparent_temperature}°C`;
+    const weather = await this.weather.getWeather();
+    Animate.decryptionAnimation(
+      this.currentTempElement,
+      `${weather.current?.temperature_2m}°C`,
+    );
+    this.windSpeedElement.innerText = `${weather.current?.wind_speed_10m} km/h`;
+    this.humidityElement.innerText = `${weather.current?.relative_humidity_2m}%`;
+    this.feelsLikeElement.innerText = `${weather.current?.apparent_temperature}°C`;
 
     this.weatherIconElement.src = this.weather.getWeatherIcon(
       weather.current?.weather_code || 0,
@@ -50,7 +63,15 @@ export class WeatherUI {
         hour12: true,
       });
 
-      li.innerText = `${time} - ${entry.temperature.toFixed(2)}°C`;
+      li.innerHTML = `
+        <div style="display: flex; align-items: center; gap: 0.5rem">
+          ${timeSVG}
+          <p>${time}</p>
+        </div>
+    
+        <p>${entry.temperature.toFixed(2)}°C</p>
+      `;
+      li.classList.add("hourly-entry");
       this.hourlyForecastElement.appendChild(li);
     });
   }
