c5c84db7ceac7c630f0b2519208d8137c5a20680
rainndev <182114243+rainndev@users.noreply.github.com>
2026-03-12 21:56:52 +0800

    feat: implement daily challenge feature with dynamic text display

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
+import challenges from "./data/daily-challenge.json";
 import { MainUI } from "./js/MainUI";
 
 const mainUI = new MainUI();
 mainUI.render();
+
+const dailyChallengeText = document.getElementById(
+  "daily-challenge-text",
+) as HTMLParagraphElement;
+
+const today = new Date().toDateString();
+
+const index =
+  today.split("").reduce((a, b) => a + b.charCodeAt(0), 0) % challenges.length;
+
+const todayChallenge = challenges[index];
+
+dailyChallengeText!.textContent = todayChallenge;
